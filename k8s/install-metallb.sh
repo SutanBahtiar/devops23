d3a43308bee73abc6724502316c464eb856d614b
Sutan Bahtiar <sutanirojim@gmail.com>
2025-03-27 12:34:30 +0700

    Update install-metallb.sh

--- k8s/install-metallb.sh
+++ k8s/install-metallb.sh
@@ -12,4 +12,12 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.9/confi
 
 kubectl apply -f https://raw.githubusercontent.com/SutanBahtiar/devops23/refs/heads/master/k8s/metallb-IPAddressPool.yaml
 
+# kubectl -n metallb-system get IPAddressPool
+# NAME         AUTO ASSIGN   AVOID BUGGY IPS   ADDRESSES
+# first-pool   true          false             ["10.50.50.50-10.50.50.100"]
+
 kubectl apply -f https://raw.githubusercontent.com/SutanBahtiar/devops23/refs/heads/master/k8s/metallb-L2Advertisement.yaml
+
+# kubectl -n metallb-system get L2Advertisement
+# NAME         IPADDRESSPOOLS   IPADDRESSPOOL SELECTORS   INTERFACES
+# sandbox-l2   ["first-pool"]
